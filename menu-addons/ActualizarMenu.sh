5312e53785f72ed55d4c9f190b7e1698de7e2168
julenvitoria <julenvitoria@users.noreply.github.com>
2022-01-17 00:30:31 +0100

    update

--- menu-addons/ActualizarMenu.sh
+++ menu-addons/ActualizarMenu.sh
@@ -49,6 +49,15 @@ else
         sudo sed -i 's|</gameList>|\t<game>\n\t\t<path>./#Menu-Addons/ConfigRemaps.sh</path>\n\t\t<name>Configuraciones y remapeos</name>\n\t\t<desc>Script para descargar/actualizar configuraciones de emuladores y mapeos de diversos juegos y sistemas. ¡¡¡ATENCION: Si has realizado cambios en las configuraciones o remapeos y ejecutas esta operacion perderas dichos cambios si al descargarse se machacan los archivos. En este caso se recomienda realizar una copia de seguridad antes de ejecutarlo!!!</desc>\n\t\t<image></image>\n\t\t<playcount>0</playcount>\n\t\t<lastplayed>20180514T205700</lastplayed>\n\t</game>\n</gameList>|' /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
 fi
 
+#Instalar restaurar sistemas
+wget -O- https://raw.githubusercontent.com/julenvitoria/6goldencoins/main/menu-addons/restoresystems.sh>/home/pi/RetroPie/retropiemenu/#Menu-Addons/restoresystems.sh
+chmod +x /home/pi/RetroPie/retropiemenu/#Menu-Addons/restoresystems.sh
+if grep -q "restoresystems.sh" /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml ; then
+        echo "Restore Systems ya se encuentra gamelist.xml"
+else
+        sudo sed -i 's|</gameList>|\t<game>\n\t\t<path>./#Menu-Addons/restoresystems.sh</path>\n\t\t<name>Restaurar sistemas de EmulationStation</name>\n\t\t<desc>Script para instalar/restaurar sistemas personalizados adicionales en es_systems.cfg después de por ejemplo una actualización a traves de retropie-setup. ¡¡¡ATENCION: SI SE HAN HECHO CAMBIOS ADICIONALES EN EL ARCHIVO es_systems.cfg UBICADO EN /opt/retropie/configs/all/emulationstation SE PERDERAN A EJECUTARSE ESTA OPCION PORQUE SE MACHACARA CON EL QUE SE DESCARGUE!!! Se recomienda realizar uñas copia de seguridad antes de ejecutar esta operación.</desc>\n\t\t<image></image>\n\t\t<playcount>0</playcount>\n\t\t<lastplayed>20180514T205700</lastplayed>\n\t</game>\n</gameList>|' /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
+fi
+
 #Crear directorio de fixes
 if [ -d /home/pi/RetroPie/retropiemenu/#Menu-Addons/Fixes ]; then
         echo "Directorio Fixes ya fue creado anteriormente... Descargando Fixes..."
@@ -61,50 +70,13 @@ else
         mkdir /home/pi/RetroPie/retropiemenu/#Menu-Addons/Fixes
 fi
 
-#Instalar fixes!!
-#Instalar gamelist & install fix
-wget -O- https://raw.githubusercontent.com/julenvitoria/Kelboy/main/patchs/installgamelist.sh>/home/pi/RetroPie/retropiemenu/#Menu-Addons/Fixes/installgamelist.sh
+#Instalar fix installgamelist
+wget -O- https://raw.githubusercontent.com/julenvitoria/6goldencoins/main/menu-addons/installgamelist.sh>/home/pi/RetroPie/retropiemenu/#Menu-Addons/Fixes/installgamelist.sh
 chmod +x /home/pi/RetroPie/retropiemenu/#Menu-Addons/Fixes/installgamelist.sh
 if grep -q "installgamelist.sh" /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml ; then
-        echo "Install with Gamelist fix ya se encuentra gamelist.xml"
+        echo "El fix installgamelist ya se encuentra gamelist.xml"
 else
         sudo sed -i 's|</gameList>|\t<game>\n\t\t<path>./#Menu-Addons/Fixes/installgamelist.sh</path>\n\t\t<name>Reinstalar Addons-Menu con gamelist original de retropie</name>\n\t\t<desc>Script para reinstalar el menu de addons descargando el gamelist del menu retropie original (necesario si cambio los nombres de los addons en gamelist.xml).</desc>\n\t\t<image></image>\n\t\t<playcount>0</playcount>\n\t\t<lastplayed>20180514T205700</lastplayed>\n\t</game>\n</gameList>|' /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
-fi
-
-#Install Restore systems fix
-wget -O- https://raw.githubusercontent.com/julenvitoria/Kelboy/main/patchs/restoresystems.sh>/home/pi/RetroPie/retropiemenu/#Menu-Addons/Fixes/restoresystems.sh
-chmod +x /home/pi/RetroPie/retropiemenu/#Menu-Addons/Fixes/restoresystems.sh
-if grep -q "restoresystems.sh" /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml ; then
-        echo "Restore Systems ya se encuentra gamelist.xml"
-else
-        sudo sed -i 's|</gameList>|\t<game>\n\t\t<path>./#Menu-Addons/Fixes/restoresystems.sh</path>\n\t\t<name>Restore systems</name>\n\t\t<desc>ENG: Script to restore aditional custom systems in es_systems.cfg after an update of the retropie setup script for example.\nESP: Script para restaurar sistemas personalizados adicionales en es_systems.cfg después de una actualización del script de configuración de retropie, por ejemplo.</desc>\n\t\t<image></image>\n\t\t<playcount>0</playcount>\n\t\t<lastplayed>20180514T205700</lastplayed>\n\t</game>\n</gameList>|' /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
-fi
-
-#Install audio fix
-wget -O- https://raw.githubusercontent.com/julenvitoria/Kelboy/main/patchs/audiofix.sh>/home/pi/RetroPie/retropiemenu/#Menu-Addons/Fixes/audiofix.sh
-chmod +x /home/pi/RetroPie/retropiemenu/#Menu-Addons/Fixes/audiofix.sh
-if grep -q "audiofix.sh" /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml ; then
-        echo "Audio fix ya se encuentra gamelist.xml"
-else
-        sudo sed -i 's|</gameList>|\t<game>\n\t\t<path>./#Menu-Addons/Fixes/audiofix.sh</path>\n\t\t<name>Fix Sound Settings</name>\n\t\t<desc>ENG: Script to fix sound settings after an update of retropie.\nESP: Script para arreglar los ajustes de sonido despues de una actualizacion de retropie.</desc>\n\t\t<image></image>\n\t\t<playcount>0</playcount>\n\t\t<lastplayed>20180514T205700</lastplayed>\n\t</game>\n</gameList>|' /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
-fi
-
-#Install restore plymouth splashscreen fix
-wget -O- https://raw.githubusercontent.com/julenvitoria/Kelboy/main/patchs/restoreplymouth.sh>/home/pi/RetroPie/retropiemenu/#Menu-Addons/Fixes/restoreplymouth.sh
-chmod +x /home/pi/RetroPie/retropiemenu/#Menu-Addons/Fixes/restoreplymouth.sh
-if grep -q "restoreplymouth.sh" /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml ; then
-        echo "Restore Pkymouth splascreen fix ya se encuentra gamelist.xml"
-else
-        sudo sed -i 's|</gameList>|\t<game>\n\t\t<path>./#Menu-Addons/Fixes/restoreplymouth.sh</path>\n\t\t<name>Restore Plymouth splash</name>\n\t\t<desc>ENG: Script to restore Plymouth splashscreen after an update of retropie.\nESP: Script para restaurar el slplash Plymouth despues de una actualizacion de retropie.</desc>\n\t\t<image></image>\n\t\t<playcount>0</playcount>\n\t\t<lastplayed>20180514T205700</lastplayed>\n\t</game>\n</gameList>|' /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
-fi
-
-#Install launcher resolution menu fix
-wget -O- https://raw.githubusercontent.com/julenvitoria/Kelboy/main/patchs/launcher-res.sh>/home/pi/RetroPie/retropiemenu/#Menu-Addons/Fixes/launcher-res.sh
-chmod +x /home/pi/RetroPie/retropiemenu/#Menu-Addons/Fixes/launcher-res.sh
-if grep -q "launcher-res.sh" /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml ; then
-        echo "Launcher resolution menu fix ya se encuentra gamelist.xml"
-else
-        sudo sed -i 's|</gameList>|\t<game>\n\t\t<path>./#Menu-Addons/Fixes/launcher-res.sh</path>\n\t\t<name>Launcher resolution menu</name>\n\t\t<desc>ENG: Menu to change the resolution of the kelboy launcher.\nESP: Menu para cambiar la resolucion del launcher de la kelboy.</desc>\n\t\t<image></image>\n\t\t<playcount>0</playcount>\n\t\t<lastplayed>20180514T205700</lastplayed>\n\t</game>\n</gameList>|' /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
 fi
 
 #Restart EmulationStation
