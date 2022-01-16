fddaece1d5e4c05b8bd40d672dc325fcb0c69ecc
julenvitoria <julenvitoria@users.noreply.github.com>
2022-01-17 00:49:20 +0100

    ActualizarMenu.sh

--- menu-addons/ActualizarMenu.sh
+++ menu-addons/ActualizarMenu.sh
@@ -29,7 +29,7 @@ chmod +x /home/pi/RetroPie/retropiemenu/#Menu-Addons/UpdateUpgrade.sh
 if grep -q "UpdateUpgradeSystem.sh" /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml ; then
         echo "El script update y upgrade al sistema ya se encuentra gamelist.xml"
 else
-        sudo sed -i 's|</gameList>|\t<game>\n\t\t<path>./#Menu-Addons/UpdateUpgrade.sh</path>\n\t\t<name>Actualizar sistema operativo</name>\n\t\t<desc>Script para actualizar los paquetes del sistema de manera con un solo click ¡¡¡ATENCION: USA BAJO TU PROPIA RESPONSABILIDAD!!!</desc>\n\t\t<image></image>\n\t\t<playcount>0</playcount>\n\t\t<lastplayed>20180514T205700</lastplayed>\n\t</game>\n</gameList>|' /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
+        sudo sed -i 's|</gameList>|\t<game>\n\t\t<path>./#Menu-Addons/UpdateUpgradeSystem.sh</path>\n\t\t<name>Actualizar sistema operativo</name>\n\t\t<desc>Script para actualizar los paquetes del sistema de manera con un solo click ¡¡¡ATENCION: USA BAJO TU PROPIA RESPONSABILIDAD!!!</desc>\n\t\t<image></image>\n\t\t<playcount>0</playcount>\n\t\t<lastplayed>20180514T205700</lastplayed>\n\t</game>\n</gameList>|' /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
 fi
 #Instalar actualizador emulators.cfg
 wget -O- https://raw.githubusercontent.com/julenvitoria/6goldencoins/main/menu-addons/ActualizarEmulators.sh>/home/pi/RetroPie/retropiemenu/#Menu-Addons/UpdateEmulators.sh
@@ -37,7 +37,7 @@ chmod +x /home/pi/RetroPie/retropiemenu/#Menu-Addons/ActualizarEmulators.sh
 if grep -q "ActualizarEmulators.sh" /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml ; then
         echo "El script para actualizar el archivo emulators.cfg ya se encuentra gamelist.xml"
 else
-        sudo sed -i 's|</gameList>|\t<game>\n\t\t<path>./#Menu-Addons/UpdateEmulators.sh</path>\n\t\t<name>Descargar emulators.cfg originalfile</name>\n\t\t<desc>Script para descargar el archivo emulators.cfg original de la imagen. Esto significa qur tambien cambiara en el caso de que se realice algun cambio para mejorar la emulacion en algun juego. ¡¡¡ATENCION: Si has realizado cambios en la eleccion de emuladores y ejecutas esta operacion perderas dichos cambios si no existen en el archivo descargado. En este caso se recomienda realizar una copia de seguridad antes de ejecutarlo!!!</desc>\n\t\t<image></image>\n\t\t<playcount>0</playcount>\n\t\t<lastplayed>20180514T205700</lastplayed>\n\t</game>\n</gameList>|' /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
+        sudo sed -i 's|</gameList>|\t<game>\n\t\t<path>./#Menu-Addons/UpdateEmulators.sh</path>\n\t\t<name>Descargar emulators.cfg original</name>\n\t\t<desc>Script para descargar el archivo emulators.cfg original de la imagen. Esto significa qur tambien cambiara en el caso de que se realice algun cambio para mejorar la emulacion en algun juego. ¡¡¡ATENCION: Si has realizado cambios en la eleccion de emuladores y ejecutas esta operacion perderas dichos cambios si no existen en el archivo descargado. En este caso se recomienda realizar una copia de seguridad antes de ejecutarlo!!!</desc>\n\t\t<image></image>\n\t\t<playcount>0</playcount>\n\t\t<lastplayed>20180514T205700</lastplayed>\n\t</game>\n</gameList>|' /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
 fi
 
 #Instalar ConfigsRemaps
@@ -46,7 +46,7 @@ chmod +x /home/pi/RetroPie/retropiemenu/#Menu-Addons/ConfigsRemaps.sh
 if grep -q "ConfigsRemaps.sh" /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml ; then
         echo "ConfigsRemaps ya se encuentra gamelist.xml"
 else
-        sudo sed -i 's|</gameList>|\t<game>\n\t\t<path>./#Menu-Addons/ConfigRemaps.sh</path>\n\t\t<name>Configuraciones y remapeos</name>\n\t\t<desc>Script para descargar/actualizar configuraciones de emuladores y mapeos de diversos juegos y sistemas. ¡¡¡ATENCION: Si has realizado cambios en las configuraciones o remapeos y ejecutas esta operacion perderas dichos cambios si al descargarse se machacan los archivos. En este caso se recomienda realizar una copia de seguridad antes de ejecutarlo!!!</desc>\n\t\t<image></image>\n\t\t<playcount>0</playcount>\n\t\t<lastplayed>20180514T205700</lastplayed>\n\t</game>\n</gameList>|' /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
+        sudo sed -i 's|</gameList>|\t<game>\n\t\t<path>./#Menu-Addons/ConfigsRemaps.sh</path>\n\t\t<name>Configuraciones y remapeos</name>\n\t\t<desc>Script para descargar/actualizar configuraciones de emuladores y mapeos de diversos juegos y sistemas. ¡¡¡ATENCION: Si has realizado cambios en las configuraciones o remapeos y ejecutas esta operacion perderas dichos cambios si al descargarse se machacan los archivos. En este caso se recomienda realizar una copia de seguridad antes de ejecutarlo!!!</desc>\n\t\t<image></image>\n\t\t<playcount>0</playcount>\n\t\t<lastplayed>20180514T205700</lastplayed>\n\t</game>\n</gameList>|' /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
 fi
 
 #Instalar restaurar sistemas
@@ -59,24 +59,24 @@ else
 fi
 
 #Crear directorio de fixes
-if [ -d /home/pi/RetroPie/retropiemenu/#Menu-Addons/Fixes ]; then
-        echo "Directorio Fixes ya fue creado anteriormente... Descargando Fixes..."
+if [ -d /home/pi/RetroPie/retropiemenu/#Menu-Addons/#fixes ]; then
+        echo "Directorio fixes ya fue creado anteriormente... Borrando y descargando de nuevo..."
         sleep 2
-        rm -R /home/pi/RetroPie/retropiemenu/#Menu-Addons/Fixes
-        mkdir /home/pi/RetroPie/retropiemenu/#Menu-Addons/Fixes
+        rm -R /home/pi/RetroPie/retropiemenu/#Menu-Addons/#fixes
+        mkdir /home/pi/RetroPie/retropiemenu/#Menu-Addons/#fixes
 else
         echo "Creando directorio Fixes y descargándolos..."
         sleep 2
-        mkdir /home/pi/RetroPie/retropiemenu/#Menu-Addons/Fixes
+        mkdir /home/pi/RetroPie/retropiemenu/#Menu-Addons/#fixes
 fi
 
 #Instalar fix installgamelist
-wget -O- https://raw.githubusercontent.com/julenvitoria/6goldencoins/main/menu-addons/installgamelist.sh>/home/pi/RetroPie/retropiemenu/#Menu-Addons/Fixes/installgamelist.sh
-chmod +x /home/pi/RetroPie/retropiemenu/#Menu-Addons/Fixes/installgamelist.sh
+wget -O- https://raw.githubusercontent.com/julenvitoria/6goldencoins/main/menu-addons/installgamelist.sh>/home/pi/RetroPie/retropiemenu/#Menu-Addons/#fixes/installgamelist.sh
+chmod +x /home/pi/RetroPie/retropiemenu/#Menu-Addons/#Fixes/installgamelist.sh
 if grep -q "installgamelist.sh" /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml ; then
         echo "El fix installgamelist ya se encuentra gamelist.xml"
 else
-        sudo sed -i 's|</gameList>|\t<game>\n\t\t<path>./#Menu-Addons/Fixes/installgamelist.sh</path>\n\t\t<name>Reinstalar Addons-Menu con gamelist original de retropie</name>\n\t\t<desc>Script para reinstalar el menu de addons descargando el gamelist del menu retropie original (necesario si cambio los nombres de los addons en gamelist.xml).</desc>\n\t\t<image></image>\n\t\t<playcount>0</playcount>\n\t\t<lastplayed>20180514T205700</lastplayed>\n\t</game>\n</gameList>|' /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
+        sudo sed -i 's|</gameList>|\t<game>\n\t\t<path>./#Menu-Addons/#Fixes/installgamelist.sh</path>\n\t\t<name>Reinstalar Addons-Menu con gamelist original de retropie</name>\n\t\t<desc>Script para reinstalar el menu de addons descargando el gamelist del menu retropie original (necesario si cambio los nombres de los addons en gamelist.xml).</desc>\n\t\t<image></image>\n\t\t<playcount>0</playcount>\n\t\t<lastplayed>20180514T205700</lastplayed>\n\t</game>\n</gameList>|' /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
 fi
 
 #Restart EmulationStation
