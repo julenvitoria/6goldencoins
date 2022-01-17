f64bb90bc4624eef317fbb6bf2f24f5988e997ae
julenvitoria <julenvitoria@users.noreply.github.com>
2022-01-17 22:05:54 +0100

    installgamelist.sh

--- menu-addons/installgamelist.sh
+++ menu-addons/installgamelist.sh
@@ -2,8 +2,8 @@
 
 echo "Downloading and copying original retropie section gamelist.xml and reinstalling Addons Menu..."
 sleep 2
-wget -O- https://raw.githubusercontent.com/julenvitoria/Kelboy/main/patchs/gamelist.xml>/opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
-wget -O- https://raw.githubusercontent.com/julenvitoria/Kelboy/main/install.sh | bash
+wget -O- https://raw.githubusercontent.com/julenvitoria/6goldencoins/main/menu-addons/patchs/gamelist.xml>/opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
+wget -O- https://raw.githubusercontent.com/julenvitoria/6goldencoins/main/menu-addons/ActualizarMenu.sh | bash
 
 echo "Install with gamelist fix applied!!!"
 sleep 3
