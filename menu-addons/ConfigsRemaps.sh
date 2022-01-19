d04a87eebf8c46623e52a654f88288b9ba4dacc3
julenvitoria <julenvitoria@users.noreply.github.com>
2022-01-19 20:22:00 +0100

    ConfigsRemaps

--- menu-addons/ConfigsRemaps.sh
+++ menu-addons/ConfigsRemaps.sh
@@ -12,6 +12,7 @@ cd configsremaps
 echo "Descargando y copiando configuraciones y remapeos de sistemas al directorio configs..."
 sleep 2
 /home/pi/scripts/github-downloader.sh https://github.com/julenvitoria/6goldencoins/tree/master/configs
+echo "Copiando archivos, por favor espera un momento..."
 cp -R configs /opt/retropie/
 cd ..
 rm -r configsremaps
