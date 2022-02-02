eb1779ea8c434d0b62b4bcad052c3097c9b438f2
julenvitoria <julenvitoria@hotmail.es>
2022-02-02 12:45:26 +0100

    Update emulationstation.sh

--- menu-addons/emulationstation.sh
+++ menu-addons/emulationstation.sh
@@ -10,13 +10,28 @@ if [ -d "/home/pi/scripts/tmp/" ]; then
     rm -r "/home/pi/scripts/tmp/"
     sleep 1
 fi
-echo "Descargando core lr-vice y aplicando fix..."
+echo "Descargando binario de ES en español y aplicando fix..."
 echo ""
 mkdir /home/pi/scripts/tmp/
 cd /home/pi/scripts/tmp/
-/home/pi/scripts/github-downloader.sh https://github.com/julenvitoria/6goldencoins/tree/master/menu-addons/patchs/lr-vice
-sudo rm -R /opt/retropie/libretrocores/lr-vice
-sudo cp -R lr-vice /opt/retropie/libretrocores/
+/home/pi/scripts/github-downloader.sh https://github.com/julenvitoria/6goldencoins/tree/master/menu-addons/patchs/emulationstation
+chmod +x emulationstation
+if [ -f "/home/pi/scripts/tmp/emulationstation" ]; then
+    /home/pi/scripts/multi_switch.sh --es-systemd
+    sleep 1
+    sudo cp emulationstation /opt/retropie/supplementary/emulationstation/
+    echo ""
+    echo "El sistema se reiniciará en 3 segundos..."
+    sleep 3
+    cd ~
+    rm -r "/home/pi/scripts/tmp/"
+    sleep 0.5
+    sudo reboot
+else
+    echo ""
+    echo "El binario de ES no se descargó... nada que aplicar"
+    sleep 2
+fi
 cd ~
 rm -r "/home/pi/scripts/tmp/"
 sleep 2
