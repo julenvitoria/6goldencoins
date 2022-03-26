b5779198eb636868ab90b2bbe8df3ca659dab3fb
julenvitoria <36037023+julenvitoria@users.noreply.github.com>
2022-03-26 22:16:39 +0100

    dosbox

--- menu-addons/dosbox.sh
+++ menu-addons/dosbox.sh
@@ -23,10 +23,13 @@ echo "Descargando core lr-gw y aplicando fix..."
 echo ""
 mkdir /home/pi/scripts/tmp/
 cd /home/pi/scripts/tmp/
-/home/pi/scripts/github-downloader.sh https://github.com/julenvitoria/6goldencoins/tree/master/menu-addons/patchs/lr-gw
-sudo rm -R /opt/retropie/libretrocores/lr-gw
-sudo cp -R lr-gw /opt/retropie/libretrocores/
-sudo chmod +x /opt/retropie/libretrocores/lr-gw/gw_libretro.so
+/home/pi/scripts/github-downloader.sh https://github.com/julenvitoria/6goldencoins/tree/master/menu-addons/patchs/DOSBox
+cp -R DOSBox /opt/retropie/configs/all/retroarch/config/
 cd ~
 rm -r "/home/pi/scripts/tmp/"
+rm /opt/retropie/configs/all/retroarch/config/DOSBox/DOSBox.opt
+#echo "config_save_on_exit = \"true\"" > /opt/retropie/configs/all/retroarch/config/DOSBox/DOSBox.cfg
+cp /home/pi/RetroPie/roms/pc/INSTALL.SYS /home/pi/RetroPie/roms/msdos
+sed -i 's|.*input_remapping_directory.*|input_remapping_directory = "/opt/retropie/configs/pc/"|' /opt/retropie/configs/pc/retroarch.cfg
+sed -i 's|.*dosbox_pcspeaker =.*|dosbox_pcspeaker = "true"|' /opt/retropie/configs/pc/retroarch-core-options.cfg
 sleep 2
